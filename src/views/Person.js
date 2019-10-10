3a50bf6047be1ecdd766394c04c62b0f6d0c6332
wallowyou <18702513938@163.com>
2019-10-10 17:05:00 +0800

    初始化

---
+++ src/views/Person.js
@@ -0,0 +1,21 @@
+import React from 'react';
+export default class Person extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return (
+      <div>
+        <h2>这是Person页面</h2>
+      </div>
+    )
+  }
+}
+function Pdetail() {
+  return (
+    <div>
+      <h2>这是</h2>
+    </div>
+  );
+}
+
